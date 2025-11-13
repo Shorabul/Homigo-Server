1cd534adb58ed5dd1ac9ef208175e2b6cd7df3a4
Shorabul Hoque <shorabulhoque@yahoo.com>
2025-11-13 18:57:47 +0100

    fix: move secrets to .env and update gitignore

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const verifyIdFierbaseToken = async (req, res, next) => {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         const db = client.db('homigo_db');
         const servicesCollection = db.collection('services');
@@ -295,7 +295,7 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Backend running...");
     } finally {
         // await client.close();
